f6a225a94bd13a6413614acfa14d9bb2d03541a5
Yash Gupta <yashgupta5532@gmail.com>
2026-02-11 21:23:27 +0530

    initialized backend repository ,added database connection and manages express server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => console.log("Server running on port 5000"));
